7c543f56ac56a230ff7afc257fc133449dd0abb0
ACONCEPT <joesadaka@gmail.com>
2018-04-30 08:39:33 -0500

    example code

---
+++ producer.py
@@ -0,0 +1,17 @@
+from pyspark import SparkConf, SparkContext
+
+APP_NAME = "My Spark Application"
+
+def main(sc):
+	rdd = sc.parallelize(range(1000), 10)
+	print rdd.mean()
+
+if __name__ == "__main__":
+    # Configure OPTIONS
+    conf = SparkConf().setAppName(APP_NAME)
+    conf = conf.setMaster("local[*]")
+    #in cluster this will be like
+    #"spark://ec2-0-17-03-078.compute-#1.amazonaws.com:7077"
+    sc   = SparkContext(conf=conf)
+    # Execute Main functionality
+    main(sc)
