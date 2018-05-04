160fc82f768b018cf7233923017cc4ee3d657b9e
ACONCEPT <joesadaka@gmail.com>
2018-05-03 23:14:34 -0500

    idk

--- producer.py
+++ producer.py
@@ -1,27 +1,48 @@
+import os
 import sys
-from pyspark import SparkContext
-from pyspark import SparkConf
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
-from pyspark.sql.context import SQLContext
+sys.path.append(os.environ["PROJECT_HOME"])
+import psycopg2
+from postgreslib.database_connection import DBConnection
+from config.config import BOOTSTRAP_SERVERS,ZOOKEEPER_SERVERS,TESTING_SERVER
+from config.database_connections import source_databases
+from helpers.kafka import KafkaWriter, get_topic
+import json
+
+class IngestionProducer(KafkaWriter):
+    def __init__(self,bootstrap_servers,datasource):
+        super().__init__(bootstrap_servers)
+        self.datasource = datasource
+        self.db = DBConnection(datasource)
+
+    def publish_to_topic(self, datasource, table, data):
+        self.producer.send(topic,json.dumps(data))
+        self.producer.flush()
+
+    def get_ingestion_data(self,table):
+        self.data , self.header = self.db.stream_table(table)
+
+    def ingest_data(self,table):
+        self.get_ingestion_data(table)
+        generator,header = self.db.stream_table("sales_orders")
+        data = {}
+
+        data["meta"] = {"table":table}
+        data["records"] = {}
+        topic = get_topic(self.datasource,table)
+        print("streaming data from table {} to topic {}".format(table,topic))
+        for record in generator:
+            data["record"] = {str(h.name):str(v) for h,v in zip(header,record)}
+            self.producer.send(topic,json.dumps(data))
+
+def main(bootstrap_servers,table):
+    print("main table {}".format(table))
+    producer = IngestionProducer(bootstrap_servers,"test_database")
+    producer.ingest_data(table)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: kafka_wordcount.py <zk> <topic>", file=sys.stderr)
-        exit(-1)
+    if "joe" in os.environ.get("HOME"):
+        print("setting bootstrap to localhost in producer")
+        bootstrap_servers = TESTING_SERVER
+    topic = sys.argv[1].strip()
+    main(bootstrap_servers,topic)
 
-    sc = SparkContext(appName="PythonStreamingKafkaWordCount")
-    ssc = StreamingContext(sc, 1)
-
-    zkQuorum, topic = sys.argv[1:]
-    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
-    lines = kvs.map(lambda x: x[1])
-    lines.pprint()
-
-    counts = lines.flatMap(lambda line: line.split(" ")) \
-                  .map(lambda word: (word, 1)) \
-                  .reduceByKey(lambda a, b: a+b)
-    counts.pprint()
-
-    ssc.start()
-    ssc.awaitTermination()
