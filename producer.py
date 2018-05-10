6e5f16b1d2a646d8e7c20becb871e39da8c0e9af
Ubuntu <ubuntu@ip-172-31-31-189.us-west-2.compute.internal>
2018-05-10 19:33:47 +0000

    stats

--- producer.py
+++ producer.py
@@ -1,13 +1,14 @@
-import psycopg2
 from postgreslib.database_connection import DBConnection
 from helpers.kafka import KafkaWriter, get_topic
 import json
 
 class IngestionProducer(KafkaWriter):
-    def __init__(self,bootstrap_servers,datasource):
+    def __init__(self,bootstrap_servers,datasource,number = False):
         super().__init__(bootstrap_servers)
+        print("starting injestion producer for number limit :  {}".format(number))
         self.datasource = datasource
         self.db = DBConnection(datasource)
+        self.number = number
 
     def publish_to_topic(self, datasource, table, data):
         self.jsonproducer.send(topic,json.dumps(data))
@@ -25,17 +26,21 @@ class IngestionProducer(KafkaWriter):
         print("streaming data from table {} to topic {}".format(table,topic))
         x = 0
         stat = {}
-        stat["topic"] = topic
+#        stat["topic"] = topic
         for i,record in enumerate(generator):
             data["record"] = {str(h.name):str(v) for h,v in zip(header,record)}
-            self.jsonproducer.send(topic,json.dumps(data))
+            self.produce(data,topic)
+#            self.jsonproducer.send(topic,json.dumps(data))
             x = i
-            if x % 1000 == 0:
-                stat["count"] = x
-                self.produce_stats(json.dumps(stat))
+#            if x % 1000 == 0:
+#                stat["count"] = x
+#                self.produce_stats(json.dumps(stat))
+            if self.number:
+                if x >= self.number:
+                    break
 
         self.produce_debug("completed producing {}, {} records".format(table,x))
-        self.produce_stats(json.dumps(stat))
+#        self.produce_stats(stat)
 
 def main(bootstrap_servers,db,table):
     print("main table {}".format(table))
